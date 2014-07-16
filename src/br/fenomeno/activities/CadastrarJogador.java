01c35a15373982cd2e5644f8ff9c0b18f2c161f2
jinduarte <jeanduarte@gmail.com>
2014-07-16 14:07:48 -0400

    Cadastro de jogadores persistido.

--- src/br/fenomeno/activities/CadastrarJogador.java
+++ src/br/fenomeno/activities/CadastrarJogador.java
@@ -1,7 +1,17 @@
 package br.fenomeno.activities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import br.fenomeno.dao.JogadorDAO;
 import br.fenomeno.entity.Jogador;
@@ -9,7 +19,7 @@ import br.fenomeno.entity.Jogador;
 
 public class CadastrarJogador extends Activity {
 	
-	private TextView textViewNome;
+	private EditText editTextNome, editTextEmail, editTextTelefone;
 
     Jogador jogador = new Jogador();
     JogadorDAO jogadorDao = new JogadorDAO(this);
@@ -18,14 +28,52 @@ public class CadastrarJogador extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.cadastro_jogadores);
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		Button cadastrarJogador = (Button) findViewById(R.id.buttonCadastrarJogador);
+		
+		cadastrarJogador.setOnClickListener(new OnClickListener() {
 
-		/**
-		 * SELECIONAR QTD DE GOLS
-		 */
-		textViewNome = (TextView)findViewById(R.id.textViewNome);
-		//jogador.getClass()
-        
+			@Override
+			public void onClick(View v) {
 
+				/**
+				 * Nome
+				 */
+				editTextNome = (EditText)findViewById(R.id.editTextNome);
+				jogador.setNome(editTextNome.getText().toString());
+				
+				/**
+				 * Email
+				 */
+				editTextEmail = (EditText)findViewById(R.id.editTextEmail);
+				jogador.setEmail(editTextEmail.getText().toString());
+				
+				/**
+				 * Telefone
+				 */
+				editTextTelefone = (EditText)findViewById(R.id.editTextTelefone);
+				jogador.setCelular(editTextTelefone.getText().toString());
+				
+				jogadorDao.salvar(jogador);
+				
+				List<Jogador> todosJogadores = new ArrayList<>();
+				todosJogadores = jogadorDao.buscarTodosJogadores();
+				
+				Intent mudarTela = new Intent(CadastrarJogador.this, Configuracoes.class);
+				startActivity(mudarTela);
+				
+				for(Jogador jog : todosJogadores){
+					
+					Log.i("Nome", jog.getNome().toString());
+				}
+				
+			}
+		});
 	}
 
 }
